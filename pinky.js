a079ef80f8a0d459bbf1c082cc1b62f1cb16c194
sazgar1 <81472492+sazgar1@users.noreply.github.com>
2021-04-05 16:22:25 +0300

    Update pinky.js

--- pinky.js
+++ pinky.js
@@ -48,46 +48,6 @@ client.on("message", message => {
 });
 
 client.on("message", message => {
-  if (message.content.startsWith(prefix + "boy", "Boy")) {
-    let boy = [
-      "https://media.discordapp.net/attachments/786897044483604490/803870769313480714/Enes_Acar_GIF_70.gif",
-      "https://media.discordapp.net/attachments/786897044483604490/803870793716858880/a_57a7f6c875e3a329b170edf177392911.gif",
-      "https://media.discordapp.net/attachments/786897044483604490/803870817351368734/5-2.gif",
-      "https://media.discordapp.net/attachments/786897044483604490/804007829010513966/image1.gif",
-      "https://media.discordapp.net/attachments/786897044483604490/804007829483552838/image3.gif",
-      "https://media.discordapp.net/attachments/786897044483604490/804219672513478706/Lenora_36.gif",
-      "https://media.discordapp.net/attachments/786897044483604490/804220384899498064/Lenora_28.gif",
-      "https://media.discordapp.net/attachments/786897044483604490/804220394697392158/Lenora_33.gif",
-      "https://media.discordapp.net/attachments/786897044483604490/804315371271749662/image0-20.gif",
-      "https://media.discordapp.net/attachments/786897044483604490/804968359572930580/ALANIS_MAN_GIF_156.gif",
-      "https://media.discordapp.net/attachments/786897044483604490/804968381816111124/image0-5.gif",
-      "https://media.discordapp.net/attachments/786897044483604490/804760463044640808/ALANIS_MAN_GIF_99.gif",
-      "https://media.discordapp.net/attachments/786897044483604490/803870704999202836/ENES_ACAR_GIF_104.gif",
-      "https://media.discordapp.net/attachments/786897044483604490/803870737941135421/ENES_ACAR_GIF_15.gif",
-      "https://media.discordapp.net/attachments/786897044483604490/803870682479067166/ENES_ACAR_GIF_135.gif",
-      "https://cdn.discordapp.com/attachments/737803513052266622/827991495331020840/image0.gif",
-      "https://cdn.discordapp.com/attachments/737803513052266622/824734156617547806/image1-1.gif",
-      "https://cdn.discordapp.com/attachments/737803513052266622/824734140448243712/image0-6.gif",
-      "https://cdn.discordapp.com/attachments/737803513052266622/824734126753316914/image0-5.gif",
-      "https://cdn.discordapp.com/attachments/737803513052266622/824734108022341673/15.gif",
-    ];
-
-    message.channel
-      .send({
-        embed: {
-          description: `${message.author.username} **BOY GIFS**  `,
-          image: {
-            url: boy[Math.floor(Math.random() * boy.length)]
-          }
-        }
-      })
-      .catch(e => {
-        client.log.error(e);
-      });
-  }
-});
-
-client.on("message", message => {
   if (message.content.startsWith(prefix + "girl", "Girl")) {
     let girl = [
       "https://media.discordapp.net/attachments/786897045436366849/804968189892755456/image2-1.gif",
